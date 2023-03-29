2eeac66c0bf8cf0d521c3d04d84a892022f7b522
sherrytetich <sheryl@kabarak.ac.ke>
2023-03-30 00:56:25 +0300

    changes

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ $result = mysqli_query($con,$query)
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <title>Fetch data from database</title>
+    <title>Registered Users</title>
 </head>
 <body class="bg-dark">
     <div class="container">
@@ -21,7 +21,7 @@ $result = mysqli_query($con,$query)
             <div class="col">
                 <div class="card mt-5">
                     <div class="card-header">
-                        <h2 class="display-6 text-center">Fetch Data From Php Database</h2>
+                        <h2 class="display-6 text-center">Relief Food Registered Users</h2>
                     </div>
                     <div class="card-body">
                         <table class="table table-bordered">
@@ -30,6 +30,8 @@ $result = mysqli_query($con,$query)
                                 <td>No.of dependants</td>
                                 <td>Phone</td>
                                 <td>Location</td>
+                                <td>Edit</td>
+                                <td>Delete</td>
                             </tr>
                             <tr>
                                 <?php
@@ -40,6 +42,8 @@ $result = mysqli_query($con,$query)
                                     <td><?php echo $row['number of dependants']?></td>
                                     <td><?php echo $row['phone']?></td>
                                     <td><?php echo $row['location']?></td>
+                                     <td><a href="#" class="btn btn-primary">Edit</a></td>
+                                    <td><a href="#" class="btn btn-danger">Delete</a></td>
                             </tr>
                                 <?php
                                 }
